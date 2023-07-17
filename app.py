48df2f00f27930b0936743edd3fee96f1f2cbd15
Justin <justin106@protonmail.com>
2023-07-17 16:55:40 -0400

    htsget local ingest + refactoring

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
 from connexion import FlaskApp
 import katsu_ingest
-import htsget_s3_ingest
+import htsget_ingest
 
 def create_app():
     connexionApp = FlaskApp(__name__)
     app = connexionApp.app
     app.register_blueprint(katsu_ingest.ingest_blueprint)
-    app.register_blueprint(htsget_s3_ingest.ingest_blueprint)
+    app.register_blueprint(htsget_ingest.ingest_blueprint)
     return app
 
 if __name__ == '__main__':
