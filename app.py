044c60f8e6e3835dfadb235402aedef882bbad01
Justin <justin106@protonmail.com>
2023-07-26 12:11:10 -0400

    Change service info endpoint

--- app.py
+++ app.py
@@ -4,20 +4,29 @@ import htsget_ingest
 
 VERSION = "2.0.0"
 
-def info():
-    info_string =  f"CanDIG ingest microservice {VERSION} "
-    if __name__ == 'main':
-        info_string += "running in development"
-    else:
-        info_string += "running on production"
-    return info_string
+def get_service_info():
+    return {
+        "id": "org.candig.drs",
+        "name": "CanDIG baby DRS service",
+        "type": {
+            "group": "org.ga4gh",
+            "artifact": "drs",
+            "version": "v1.2.0"
+        },
+        "description": "A DRS-compliant server for CanDIG genomic data",
+        "organization": {
+            "name": "CanDIG",
+            "url": "https://www.distributedgenomics.ca"
+        },
+        "version": VERSION
+    }
 
 def create_app():
     connexionApp = FlaskApp(__name__)
     app = connexionApp.app
     app.register_blueprint(katsu_ingest.ingest_blueprint)
     app.register_blueprint(htsget_ingest.ingest_blueprint)
-    app.add_url_rule('/', 'info', info)
+    app.add_url_rule('/', 'info', get_service_info)
     return app
 
 if __name__ == '__main__':
