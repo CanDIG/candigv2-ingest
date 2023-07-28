7946e2388f61b5c07199df21687fed1b8cbfbf39
Justin <justin106@protonmail.com>
2023-07-28 15:11:24 -0400

    Clinical ingest tests

--- tests/integration/test_clinical_ingest.py
+++ tests/integration/test_clinical_ingest.py
@@ -1,6 +1,8 @@
 import os
 import requests
 import pytest
+import warnings
+import json
 
 import katsu_ingest
 from auth import get_auth_header
@@ -8,94 +10,77 @@ from auth import get_auth_header
 KATSU_URL = os.environ.get("CANDIG_URL")
 
 @pytest.fixture
-def headers_admin():
+def headers():
     return get_auth_header()
 
-@pytest.fixture
-def headers_non_admin():
-    return get_auth_header(admin=False)
-
-@pytest.fixture(autouse=True)
-def setup_ingest(headers_admin):
-    data_location = "https://raw.githubusercontent.com/CanDIG/katsu/develop/chord_metadata_service/mohpackets/data/small_dataset/synthetic_data/"
-    katsu_ingest.clean_data(KATSU_URL, headers_admin)
-    katsu_ingest.ingest_data(KATSU_URL, data_location, headers_admin)
+@pytest.fixture(autouse=True, scope="module")
+def setup_ingest():
+    headers = get_auth_header()
+    if os.getenv("PROD_ENVIRONMENT") == "TRUE":
+        print("PRODUCTION ENVIRONMENT DETECTED - ABORTING TESTS\n"
+              "Ingest tests require the deletion of some datasets which can be dangerous in a production environment. "
+              "Please run them on a development environment instead.")
+        warnings.warn("Production environment detected - ingest tests will not run")
+        exit()
+    #data_location = "https://raw.githubusercontent.com/CanDIG/katsu/develop/chord_metadata_service/mohpackets/data/small_dataset/synthetic_data/"
+    requests.delete(KATSU_URL + "/katsu/v2/authorized/programs/SYNTHETIC-2/", headers=headers)
+    with open("single_ingest.json", "r") as f:
+        dataset = json.load(f)
+    katsu_ingest.ingest_donor_with_clinical(KATSU_URL, dataset, headers)
 
 def check_exists(headers, endpoint, number=-1):
-    response = requests.get("%s%s" % (KATSU_URL, endpoint),
+    response = requests.get(KATSU_URL + endpoint,
                             headers=headers)
     if (number == -1):
         assert response.json()["results"]
     else:
         assert len(response.json()["results"]) == number
 
-def check_not_exists(headers, endpoint):
-    response = requests.get("%s%s" % (KATSU_URL, endpoint),
-                            headers=headers)
-    assert not response.json()["results"]
-
 class TestClinicalIngest:
     @pytest.fixture(autouse=True)
-    def _set_admin_headers(self, headers_admin):
-        self.headers_admin = headers_admin
-    @pytest.fixture(autouse=True)
-    def _set_headers(self, headers_non_admin):
-        self.headers = headers_non_admin
+    def _set_admin_headers(self, headers):
+        self.headers = headers
 
     def test_programs(self):
-        program_1 = 'SYNTHETIC-1'
-        program_2 = 'SYNTHETIC-2'
-        check_exists(self.headers, "/katsu/v2/authorized/programs/?program_id=%s" % program_1)
-        check_exists(self.headers_admin, "/katsu/v2/authorized/programs/?program_id=%s" % program_2)
+        check_exists(self.headers, "/katsu/v2/authorized/programs/?program_id=%s" % "SYNTHETIC-2")
     def test_donors(self):
-        donor_id_1 = 'DONOR_7'
-        donor_id_2 = 'DONOR_1'
-        check_exists(self.headers_admin, "/katsu/v2/authorized/donors/?submitter_donor_id=%s" % donor_id_1)
-        check_exists(self.headers, "/katsu/v2/authorized/donors/?submitter_donor_id=%s" % donor_id_2)
+        donor_id = 'DONOR_7'
+        check_exists(self.headers, "/katsu/v2/authorized/donors/?submitter_donor_id=%s" % donor_id)
+        check_exists(self.headers, "/katsu/v2/authorized/donors/", 10)
     def test_comorbidities(self):
-        c_type = "C64.9"
-        check_exists(self.headers_admin, "/katsu/v2/authorized/comorbidities/?comorbidity_type_code=%s" % c_type)
+        check_exists(self.headers, "/katsu/v2/authorized/comorbidities/", 14)
     def test_exposures(self):
-        years_smoked = "104.0"
-        check_exists(self.headers_admin, "/katsu/v2/authorized/comorbidities/?pack_years_smoked=%s" % years_smoked)
+        check_exists(self.headers, "/katsu/v2/authorized/exposures/", 8)
     def test_specimens(self):
         s_id = "SPECIMEN_19"
-        check_exists(self.headers_admin, "/katsu/v2/authorized/specimens/?specimen_id=%s" % s_id)
         check_exists(self.headers, "/katsu/v2/authorized/specimens/?specimen_id=%s" % s_id)
+        check_exists(self.headers, "/katsu/v2/authorized/specimens/", 22)
     def test_biomarkers(self):
-        s_id = "SPECIMEN_1"
-        check_exists(self.headers, "/katsu/v2/authorized/biomarkers/?submitter_specimen_id=%s" % s_id)
-        check_not_exists(self.headers_admin, "/katsu/v2/authorized/biomarkers/?submitter_specimen_id=%s" % s_id)
+        check_exists(self.headers, "/katsu/v2/authorized/biomarkers/",  12)
     def test_follow_ups(self):
         f_id = "FOLLOW_UP_25"
-        check_exists(self.headers_admin, "/katsu/v2/authorized/follow_ups/?submitter_follow_up_id=%s" % f_id)
-        check_exists(self.headers, "/katsu/v2/authorized/follow_ups/?submitter_follow_up_id", 24)
+        check_exists(self.headers, "/katsu/v2/authorized/follow_ups/?submitter_follow_up_id=%s" % f_id)
+        check_exists(self.headers, "/katsu/v2/authorized/follow_ups/", 28)
     def test_chemotherapies(self):
-        donor_id = "DONOR_1"
-        check_exists(self.headers, "/katsu/v2/authorized/chemotherapies/?submitter_donor_id=%s" % donor_id, 7)
-        check_not_exists(self.headers_admin, "/katsu/v2/authorized/chemotherapies/?submitter_donor_id=%s" % donor_id)
+        check_exists(self.headers, "/katsu/v2/authorized/chemotherapies/", 7)
     def test_radiations(self):
         check_exists(self.headers, "/katsu/v2/authorized/radiations/", 5)
-        check_not_exists(self.headers_admin, "/katsu/v2/authorized/radiations/")
     def test_surgeries(self):
-        check_exists(self.headers, "/katsu/v2/authorized/surgeries/", 1)
-        check_exists(self.headers_admin, "/katsu/v2/authorized/surgeries/", 4)
+        check_exists(self.headers, "/katsu/v2/authorized/surgeries/", 5)
     def test_immunotherapies(self):
         check_exists(self.headers, "/katsu/v2/authorized/immunotherapies/", 7)
-        check_not_exists(self.headers_admin, "/katsu/v2/authorized/immunotherapies/")
     def test_treatments(self):
-        check_exists(self.headers, "/katsu/v2/authorized/treatments/", 18)
-        check_exists(self.headers_admin, "/katsu/v2/authorized/treatments/", 4)
+        treatment_id = "TREATMENT_1"
+        check_exists(self.headers, "/katsu/v2/authorized/treatments/?submitter_treatment_id=%s" % treatment_id)
+        check_exists(self.headers, "/katsu/v2/authorized/treatments/", 22)
     def test_hormone(self):
         check_exists(self.headers, "/katsu/v2/authorized/hormone_therapies/", 7)
-        check_not_exists(self.headers_admin, "/katsu/v2/authorized/hormone_therapies/")
     def test_sample(self):
-        check_exists(self.headers, "/katsu/v2/authorized/sample_registrations/", 24)
-        check_exists(self.headers_admin, "/katsu/v2/authorized/sample_registrations/", 4)
+        sample_id = "SAMPLE_REGISTRATION_1"
+        check_exists(self.headers, "/katsu/v2/authorized/sample_registrations/?submitter_sample_id=%s" % sample_id)
+        check_exists(self.headers, "/katsu/v2/authorized/sample_registrations/", 28)
     def test_diagnoses(self):
-        check_exists(self.headers, "/katsu/v2/authorized/primary_diagnoses/", 12)
-        check_exists(self.headers_admin, "/katsu/v2/authorized/primary_diagnoses/", 4)
-
-
-
-
+        diag_id = "PRIMARY_DIAGNOSIS_1"
+        check_exists(self.headers,
+                     "/katsu/v2/authorized/primary_diagnoses/?submitter_primary_diagnosis_id=%s" % diag_id)
+        check_exists(self.headers, "/katsu/v2/authorized/primary_diagnoses/", 16)
