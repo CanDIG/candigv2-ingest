0b86f9c56a47dbf24c1e46cdd2ec36e55be59915
Daisie Huang <daisieh@gmail.com>
2023-12-01 20:51:39 -0800

    one by one instead of bulk

--- katsu_ingest.py
+++ katsu_ingest.py
@@ -83,31 +83,32 @@ def ingest_schemas(fields, headers):
             ingest_str = f"/katsu/v2/ingest/{name}/"
             ingest_url = CANDIG_URL + ingest_str
 
-            update_headers(headers)
-            response = requests.post(
-                ingest_url, headers=headers, data=json.dumps(fields[type])
-            )
+            for item in fields[type]:
+                update_headers(headers)
+                response = requests.post(
+                    ingest_url, headers=headers, data=json.dumps(item)
+                )
 
-            if response.status_code == HTTPStatus.CREATED:
-                result["results"].append(f"Of {len(fields[type])} {type}, {response.json()['result']} were created")
-            elif response.status_code == HTTPStatus.NOT_FOUND:
-                message = f"ERROR 404: {ingest_url} was not found! Please check the URL."
-                result["errors"].append(f"{type}: {message}")
-                break
-            elif response.status_code == HTTPStatus.FORBIDDEN:
-                message = f"ERROR 403: You do not have permission to ingest {type} for {fields[type][0]['program_id']}"
-                result["errors"].append(f"{type}: {message}")
-                break
-            else:
-                try:
-                    if "error" in response.json():
-                        result["errors"].append(f"{type}: {response.status_code} {response.json()['error']}")
-                except:
-                    message = f"\nREQUEST STATUS CODE: {response.status_code} \nRETURN MESSAGE: {response.text}\n"
+                if response.status_code == HTTPStatus.CREATED:
+                    result["results"].append(f"Of {len(item)} {type}, {response.json()['result']} were created")
+                elif response.status_code == HTTPStatus.NOT_FOUND:
+                    message = f"ERROR 404: {ingest_url} was not found! Please check the URL."
                     result["errors"].append(f"{type}: {message}")
-                if type == "programs" and "unique" in response.text:
-                    # this is still okay to return 200:
-                    return result, 200
+                    break
+                elif response.status_code == HTTPStatus.FORBIDDEN:
+                    message = f"ERROR 403: You do not have permission to ingest {type} for {item[0]['program_id']}"
+                    result["errors"].append(f"{type}: {message}")
+                    break
+                else:
+                    try:
+                        if "error" in response.json():
+                            result["errors"].append(f"{type}: {response.status_code} {response.json()['error']}")
+                    except:
+                        message = f"\nREQUEST STATUS CODE: {response.status_code} \nRETURN MESSAGE: {response.text}\n"
+                        result["errors"].append(f"{type}: {message}")
+                    if type == "programs" and "unique" in response.text:
+                        # this is still okay to return 200:
+                        return result, 200
     return result, response.status_code
 
 
