f330ea63aa40143e63662bc18399ec38908c4b37
Marion <mshadbolt@users.noreply.github.com>
2024-03-14 13:14:43 -0700

    use ingest dir

--- generate_test_data.py
+++ generate_test_data.py
@@ -1,21 +1,34 @@
 from git import Repo
 import shutil
 from clinical_etl import CSVConvert
+import argparse
+import os
 
 
-def main():
-    print("Cloning mohccn-synthetic-data repo into tmp")
-    Repo.clone_from("https://github.com/CanDIG/mohccn-synthetic-data.git", "tmp")
+def parse_args():
+    parser = argparse.ArgumentParser(description="A script that ingests clinical data into Katsu")
+    parser.add_argument("--output", help="Path to clone synth data repo.", required=True)
+    args = parser.parse_args()
+    return args
+
+
+def main(args):
+    ingest_repo_dir = os.path.dirname(os.path.abspath(__file__))
+    print(f"Cloning mohccn-synthetic-data repo into {args.output}")
+    Repo.clone_from("https://github.com/CanDIG/mohccn-synthetic-data.git", args.output)
     print("Converting small_dataset_csvs to raw_data_map.json")
-    CSVConvert.csv_convert(input_path="tmp/small_dataset_csv/raw_data",
-                           manifest_file="tmp/small_dataset_csv/manifest.yml", minify=True,
+    CSVConvert.csv_convert(input_path=f"{args.output}/small_dataset_csv/raw_data",
+                           manifest_file=f"{args.output}/small_dataset_csv/manifest.yml", minify=True,
                            index_output=False)
     print("moving files to tests directory")
-    shutil.move("tmp/small_dataset_csv/raw_data_map.json", "tests/small_dataset_clinical_ingest.json")
-    shutil.move("tmp/small_dataset_csv/genomic.json", "tests/small_dataset_genomic_ingest.json")
+    shutil.move(f"{args.output}/small_dataset_csv/raw_data_map.json",
+                f"{ingest_repo_dir}/tests/small_dataset_clinical_ingest.json")
+    shutil.move(f"{args.output}/small_dataset_csv/genomic.json",
+                f"{ingest_repo_dir}/tests/small_dataset_genomic_ingest.json")
     print("Removing repo.")
-    shutil.rmtree("tmp")
+    shutil.rmtree(args.output)
 
 
 if __name__ == "__main__":
-    main()
+    args = parse_args()
+    main(args)
