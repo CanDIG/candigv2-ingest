5576480ec9606aefda5c7c0a511fa0705161f2fb
Marion <mshadbolt@users.noreply.github.com>
2024-04-08 14:56:36 -0700

    save errors if conversion fails

--- generate_test_data.py
+++ generate_test_data.py
@@ -19,24 +19,28 @@ def main(args):
     ingest_repo_dir = os.path.dirname(os.path.abspath(__file__))
     print(f"Cloning mohccn-synthetic-data repo into {args.output}")
     repo = Repo.clone_from("https://github.com/CanDIG/mohccn-synthetic-data.git", args.output)
-    # can be uncommented to check error behaviour
-    # repo.git.checkout('mshadbolt/invalid-data')
+    # can be commented to check error behaviour
+    repo.git.checkout('mshadbolt/invalid-data')
     print("Converting small_dataset_csvs to raw_data_map.json")
     try:
         packets, errors = CSVConvert.csv_convert(input_path=f"{args.output}/small_dataset_csv/raw_data",
                                                  manifest_file=f"{args.output}/small_dataset_csv/manifest.yml")
         if errors:
             raise ValidationError("Clinical etl conversion failed to create an ingestable json file, "
-                                  "please check the errors above and try again.")
+                                  "please check the errors in tests/clinical_data_validation_results.json and try again.")
     except Exception as e:
         print(e)
+        print(f"Moving validation results file to {ingest_repo_dir}/tests/clinical_data_validation_results.json.")
+        shutil.move(f"{args.output}/small_dataset_csv/raw_data_validation_results.json",
+                    f"{ingest_repo_dir}/tests/clinical_data_validation_results.json")
         print("Removing repo.")
         shutil.rmtree(args.output)
         sys.exit(0)
 
-    print("Ingestable JSON successfully created, moving json files to tests directory")
+    print("Ingestable JSON successfully created, moving output json files to tests directory")
     shutil.move(f"{args.output}/small_dataset_csv/raw_data_map.json",
                 f"{ingest_repo_dir}/tests/small_dataset_clinical_ingest.json")
+
     shutil.move(f"{args.output}/small_dataset_csv/genomic.json",
                 f"{ingest_repo_dir}/tests/small_dataset_genomic_ingest.json")
     print("Removing repo.")
