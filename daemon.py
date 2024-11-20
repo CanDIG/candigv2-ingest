309ebc7c24941de60948e66722380df0ebdc0d48
Daisie Huang <daisieh@gmail.com>
2024-11-20 11:03:32 -0800

    catch exceptions in daemon

--- daemon.py
+++ daemon.py
@@ -28,8 +28,11 @@ def ingest_file(file_path):
             json_data = json_data["katsu"]
             programs = list(json_data.keys())
             for program_id in programs:
-                ingest_results, status_code = ingest_schemas(json_data[program_id]["schemas"])
-                results[program_id] = ingest_results
+                try:
+                    ingest_results, status_code = ingest_schemas(json_data[program_id]["schemas"])
+                    results[program_id] = ingest_results
+                except Exception as e:
+                    results[program_id] = f"Exception: {type(e)} {str(e)}"
         elif "htsget" in json_data:
             do_not_index = False
             if "do_not_index" in json_data:
@@ -37,8 +40,11 @@ def ingest_file(file_path):
             json_data = json_data["htsget"]
             programs = list(json_data.keys())
             for program_id in programs:
-                ingest_results, status_code = htsget_ingest(json_data[program_id], do_not_index)
-                results[program_id] = ingest_results
+                try:
+                    ingest_results, status_code = htsget_ingest(json_data[program_id], do_not_index)
+                    results[program_id] = ingest_results
+                except Exception as e:
+                    results[program_id] = f"Exception: {type(e)} {str(e)}"
         with open(results_path, "w") as f:
             json.dump(results, f)
         os.remove(file_path)
