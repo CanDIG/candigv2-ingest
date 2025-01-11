e4164849bccf33bd62007451abcb4ffb8548ba0f
Daisie Huang <daisieh@gmail.com>
2025-01-10 19:45:48 -0800

    move aws stuff to s3_ingest

--- auth.py
+++ auth.py
@@ -35,31 +35,3 @@ def get_refresh_token(token):
         client_secret = client_secret,
         refresh_token=token
         )
-
-#####
-# AWS stuff
-#####
-
-def get_minio_client(token, s3_endpoint, bucket, access_key=None, secret_key=None, region=None, secure=True):
-    return authx.auth.get_minio_client(token=token, s3_endpoint=s3_endpoint, bucket=bucket, access_key=access_key, secret_key=secret_key, region=region, secure=secure)
-
-
-def parse_s3_credential(awsfile):
-    # parse the awsfile:
-    access = None
-    secret = None
-    with open(awsfile) as f:
-        lines = f.readlines()
-        while len(lines) > 0 and (access is None or secret is None):
-            line = lines.pop(0)
-            parse_access = re.match(r"(aws_access_key_id|AWSAccessKeyId)\s*=\s*(.+)$", line)
-            if parse_access is not None:
-                access = parse_access.group(2)
-            parse_secret = re.match(r"(aws_secret_access_key|AWSSecretKey)\s*=\s*(.+)$", line)
-            if parse_secret is not None:
-                secret = parse_secret.group(2)
-    if access is None:
-        return {"error": "awsfile did not contain access ID"}
-    if secret is None:
-        return {"error": "awsfile did not contain secret key"}
-    return {"access": access, "secret": secret}
