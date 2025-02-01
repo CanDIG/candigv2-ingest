ce73e869579edb2f94aeb69a3164462a438ba5d6
Daisie Huang <daisieh@gmail.com>
2025-01-31 16:49:09 -0800

    parse public urls

--- htsget_ingest.py
+++ htsget_ingest.py
@@ -215,11 +215,15 @@ def parse_s3_url(url):
                 "object": bucket_parse.group(2)
             }
             # check existence of credential for this:
-            response, status_code = get_s3_url(s3_endpoint=data["endpoint"], bucket=data["bucket"], object_id=data["object"], access_key=None, secret_key=None, region=None, public=False)
+            object = data["object"].split("?public=")
+            data["object"] = object[0]
+            if len(object) > 1:
+                data["object"] = object[0]
+                response, status_code = get_s3_url(s3_endpoint=data["endpoint"], bucket=data["bucket"], object_id=data["object"], access_key=None, secret_key=None, region=None, public=True)
+            else:
+                response, status_code = get_s3_url(s3_endpoint=data["endpoint"], bucket=data["bucket"], object_id=data["object"], access_key=None, secret_key=None, region=None, public=False)
+
             if status_code == 500:
-                # check to see if it is a public url
-                response2, status_code = get_s3_url(s3_endpoint=data["endpoint"], bucket=data["bucket"], object_id=data["object"], access_key=None, secret_key=None, region=None, public=True)
-                if status_code == 500:
                     raise Exception(response["error"])
             return data
         raise Exception(f"S3 URI {url} does not contain a bucket name")
