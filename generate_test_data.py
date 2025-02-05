774db81715eb3b935d734ecb82e3da444768c1eb
Marion <mshadbolt@users.noreply.github.com>
2025-02-05 11:28:08 -0800

    add katsu as arg

--- generate_test_data.py
+++ generate_test_data.py
@@ -13,6 +13,9 @@ import pprint
 def parse_args():
     parser = argparse.ArgumentParser(description="A script that copies and converts data from mohccn-synthetic-data for "
                                                  "ingest into CanDIG platform.")
+    parser.add_argument("--katsu-repo", default="lib/katsu/katsu_service/chord_metadata_service/mohpackets/data",
+                        help="Path to the katsu repo that contains jsons that should be used to generate the synth data."
+                             "Default is the standard katsu repo that is part of the stack")
     parser.add_argument("--prefix", help="optional prefix to apply to all identifiers")
     parser.add_argument("--tmp", help="Directory to temporarily clone the mohccn-synthetic-data repo.",
                         default="tmp-data")
@@ -42,7 +45,7 @@ def main(args):
 
     try:
         if args.prefix:
-            process = subprocess.run([f'python {args.tmp}/src/json_to_csv.py --input lib/katsu/katsu_service/chord_metadata_service/mohpackets/data --size s'],
+            process = subprocess.run([f'python {args.tmp}/src/json_to_csv.py --input {args.katsu_repo} --size s'],
                                      shell=True, check=True, capture_output=True)
             process = subprocess.run([f'python {args.tmp}/src/csv_to_ingest.py --size s --prefix {args.prefix}'],
                                      shell=True, check=True, capture_output=True)
