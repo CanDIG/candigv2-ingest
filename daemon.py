2f7607f652c077c41f0c7a7d508f39f446bd9748
Daisie Huang <daisieh@gmail.com>
2026-03-26 11:01:43 -0700

    list all programs in results at the start

--- daemon.py
+++ daemon.py
@@ -42,8 +42,8 @@ def ingest_file(file_path):
                     do_not_index = json_data["do_not_index"]
                 json_data = json_data["htsget"]
                 programs = list(json_data.keys())
-                # for program_id in programs:
-                #     results[program_id] = {"errors": []}
+                for program_id in programs:
+                    results[program_id] = {}
                 for program_id in programs:
                     try:
                         ingest_results, status_code = htsget_ingest(json_data[program_id], do_not_index=do_not_index, results_path=results_path, result_dict=results)
