2984483449ef275042dd6a26d732b24f2f25cb66
Daisie Huang <daisieh@gmail.com>
2026-04-21 10:34:48 -0700

    Rewrite daemon to be based on pid's existence

--- daemon.sh
+++ daemon.sh
@@ -1,4 +1,21 @@
-until python daemon.py; do
-    echo "Daemon crashed with exit code $?.  Respawning.." >&2
-    sleep 1
+#!/bin/bash
+
+python daemon.py &
+pid=$!
+
+while true
+do
+    sleep 60
+    restart_me=0 # if this is 1, respawn daemon and resave pid
+
+    if [[ $(ps -p $pid | wc -l) == 1 ]]; then
+        echo "Process $pid has terminated"
+        restart_me=1
+    fi
+
+    if [[ $restart_me == 1 ]]; then
+        echo "Restarting daemon"
+        python daemon.py &
+        pid=$!
+    fi
 done
