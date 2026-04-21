2f463e4d4d8b2c04b61e88e8257979bfd3e8cf4c
Daisie Huang <daisieh@gmail.com>
2026-04-21 11:28:13 -0700

    Update daemon.sh

--- daemon.sh
+++ daemon.sh
@@ -12,25 +12,27 @@ do
     sleep 60
     restart_me=0 # if this is 1, respawn daemon and resave pid
 
-    # check to see if the touch file exists and is still updating
-    if [[ -f $TOUCH_FILE ]]; then
-        now=$(date +%s)
-        last_touch=$(cat $TOUCH_FILE)
-        echo "Ingest in process...last updated" $(($now - $last_touch)) "seconds ago"
-        if [[ $(($now - $last_touch)) > $(($interval)) ]]; then
-            echo "Last update was more than" $interval "seconds ago"
-            rm $TOUCH_FILE
-            restart_me=1
-        fi
-    fi
-
     if [[ $(ps -p $pid | wc -l) == 1 ]]; then
         echo "Process $pid has terminated"
         restart_me=1
     fi
 
+    # check to see if the touch file exists and is still updating
+    if [[ -f $TOUCH_FILE ]]; then
+        now=$(date +%s)
+        last_touch=$(cat $TOUCH_FILE)
+        echo "Ingest in process...last updated" $(($now - $last_touch)) "seconds ago"
+        if (($now - $last_touch > $interval)); then
+            echo "Last update was more than" $interval "seconds ago"
+            kill $pid
+            restart_me=1
+        fi
+    fi
+
     if [[ $restart_me == 1 ]]; then
         echo "Restarting daemon"
+        if [[ -f $TOUCH_FILE ]]; then
+            rm $TOUCH_FILE
         python daemon.py &
         pid=$!
     fi
